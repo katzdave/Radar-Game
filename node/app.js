9fb1455702952b74cf787c14ec9d1d668efd94d1
Kevin Chen <kchen@palantir.com>
2014-07-19 13:18:15 -0700

    Add basic node framework.

---
+++ node/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var http = require('http');
+var routes = require('./routes');
+
+var app = express();
+
+// all environments
+
+
+app.set('port', '8080');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
+
+app.use(express.cookieParser());
+app.use(express.logger('dev'));
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(__dirname + '/public'));
+app.use(express.session({ secret: 'SECRET' }));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.home);
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
