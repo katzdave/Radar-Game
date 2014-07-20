583670333f7deb11e7facb26c8022a5af12ba976
David Katz <katzdave@yahoo.com>
2014-07-20 02:58:45 -0400

    get root groups

--- queries/test.sql
+++ queries/test.sql
@@ -26,21 +26,27 @@ create table Groups
 (
 	gId int NOT NULL AUTO_INCREMENT,
 	pId int,
+	Level int NOT NULL,
 	Groupname varchar(64) NOT NULL,
 	Description varchar(256),
+	ImageUrl varchar(256),
 	Password varchar(32),
-	isPublic varchar(32) NOT NULL,
+	isPublic bit NOT NULL,
+	isVisible bit NOT NULL,  
 	CONSTRAINT pk_gId PRIMARY KEY (gId)
 );
 
-insert into Groups (Groupname, isPublic)
-	values ('humans vs zombies', 1);
+insert into Groups (Groupname, isPublic, isVisible, Level)
+	values ('humans vs zombies', 1, 0, 0);
 
-insert into Groups (pId, Groupname, isPublic)
-	values (1, 'humans', 0);
+insert into Groups (Groupname, isPublic, isVisible, Level)
+	values ('field trip planner', 1, 0, 0);
 
-insert into Groups (pId, Groupname, isPublic)
-	values (1, 'zombies',0);
+insert into Groups (pId, Groupname, isPublic, isVisible, Level)
+	values (1, 'humans', 0, 1, 1);
+
+insert into Groups (pId, Groupname, isPublic, isVisible, Level)
+	values (1, 'zombies', 0, 1, 1);
 
 create table User_In_Group
 (
@@ -52,6 +58,9 @@ create table User_In_Group
 
 insert into User_In_Group (gId, uId, isAdmin)
 	values (1,1,0);
+
+insert into User_In_Group (gId, uId, isAdmin)
+	values (2,1,0);
 
 create table Rules
 (
