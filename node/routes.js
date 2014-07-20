b70768e043f2012d81dfdb337463d20cd448eb2a
Ante Qu <Ante@Antes-MacBook-Pro.local>
2014-07-19 19:57:19 -0700

    facebook login attempt

--- node/routes.js
+++ node/routes.js
@@ -5,3 +5,6 @@ exports.home = function(req, res) {
 exports.geo = function(req, res) {
   res.render('geo.html', {user: 'Alpha'});
 };
+exports.facebook = function(req, res) {
+  res.render('facebook.html');
+};
