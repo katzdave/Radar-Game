7cafa1d8dc5010c7417688757f15bbc423719c5f
Ante Qu <Ante@Antes-MacBook-Pro.local>
2014-07-19 20:45:12 -0700

    maps and login interface

--- node/sql.js
+++ node/sql.js
@@ -1,5 +1,8 @@
 var model = require('./model');
 
+/* Get functions */
+
+
 exports.getRules = function(callback){
 	query = 'select * from Rules'
 	model.execute(query, [], function(err, rows){
@@ -19,3 +22,29 @@ exports.getUsersFromGroup = function(callback, gId){
 		}
 	});
 }
+
+/* This function returns JSON object that contains a tree of all the groups under group gId.
+ *  If gId == null, this returns a JSON object of trees of all the groups. 
+ *  If gId is invalid, returns an error. */
+/* Callback format: callback(err, groupTree) */
+exports.getGroupTree = function(callback, gId) {
+
+}
+
+/* This function checks the database to see if there already is a user associated with this
+ * facebook ID. If so, return the user ID. If not, create this user as a new entry, generate
+ * an User ID for this user, and return the new user ID. */
+/* Callback format: callback(err, uId); */
+exports.getUidFromFbid = function(callback, fbId){
+
+}
+
+
+/* Set functions */
+
+/* This function updates the lattitude and longitude coordinates of the user with user ID uId. */
+/* Callback format: callback(err); */
+exports.setPosition = function(callback, uId, lat, lng){
+
+}
+
