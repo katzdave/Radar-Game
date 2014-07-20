ce667eed41db23c3ced701d61ad8f386de75afac
Ante Qu <Ante@Antes-MacBook-Pro.local>
2014-07-20 00:47:06 -0700

    undo last change

--- node/createEvent.js
+++ node/createEvent.js
@@ -1,7 +1,7 @@
 //js for first creation of event.
 
 exports.createNewEvent = function(req, res){
-	res.send(JSON.stringify(req.user));
+
   //first ask for a login
   res.render('facebook.html');
   
