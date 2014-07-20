50e8902a340c0683510f4b74099ddf3e0aae4837
Ante Qu <Ante@Antes-MacBook-Pro.local>
2014-07-20 00:46:05 -0700

    create change test

--- node/createEvent.js
+++ node/createEvent.js
@@ -1,8 +1,9 @@
 //js for first creation of event.
 
 exports.createNewEvent = function(req, res){
+	res.send(JSON.stringify(req.user));
   //first ask for a login
   res.render('facebook.html');
-
+  
 
 };
